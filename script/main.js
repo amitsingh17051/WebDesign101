086b2bc947125c2ed223172a2c4f5330b58d5817
amit singh <amitsingh17051@gmail.com>
2017-03-25 01:35:32 +0530

    Web Design 101
    This Site is for tracking my progress on web design 101 and also this is the first site of my webdesign101.

---
+++ script/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".fa-bars").on("click", function(){
+    $(".list").css({
+      "display":"block",
+      "right":"0%",
+    });
+    
+    
+  });
+  
+  $(".fa-times").on("click", function(){
+    $(".list").css({
+      "display":"none",
+      "right":"-20%",
+    });
+    
+    
+    
+  });
+  
+
+});
